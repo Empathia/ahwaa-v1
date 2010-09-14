25732437aea29d65e834c2d4b3768526cb5098db
@edgarjs <edgar.js@gmail.com>
2010-09-14 12:22:29 -0500

    First commit

---
+++ Gemfile
@@ -0,0 +1,12 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.0.0'
+gem "rails3-generators"
+gem 'mysql2'
+gem 'jquery-rails'
+
+group :test do
+  gem "rspec-rails", "2.0.0.beta.22"
+  gem "factory_girl_rails"
+end
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lgbt::Application.config.secret_token = '35b7b8bcc4b13ea3062378b47a6a8942685190613a373db22c4dc3d2af49e6726d2d95025e3fcd551aaea2d4791a3c15710805e9edfb07d5d9fe2bf894699ac8'
