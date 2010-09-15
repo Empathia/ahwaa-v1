c28726d97fb56a4364383862255d08534500f5dc
@edgarjs <edgar.js@gmail.com>
2010-09-15 16:55:33 -0500

    Adding layout helpers for js and css

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,19 @@
 module ApplicationHelper
+
+  # Wraps content to display within the +<head>+ tags
+  def head(&block)
+    content_for(:head) { block.call }
+  end
+
+  # Include javascript files within the +<head>+ tags
+  def javascript(*args)
+    args = args.map { |arg| arg == :defaults ? arg : arg.to_s }
+    head { javascript_include_tag *args }
+  end
+
+  # Include stylesheet files within the +<head>+ tags
+  def stylesheet(*args)
+    args.map! { |arg| arg.to_s }
+    head { stylesheet_link_tag *args }
+  end
 end
