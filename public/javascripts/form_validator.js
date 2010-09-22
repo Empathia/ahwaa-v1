0d7945eb108fc34138867cc0218dbcc63bf37661
felix <cf@freshout.us>
2010-09-22 10:24:14 -0700

    html5 signup form and added support for older browsers

---
+++ public/javascripts/form_validator.js
@@ -0,0 +1,26 @@
+$.fn.formValidator = function(options){
+    return this.each(function(){
+        var _form = $(this);  
+        _form.submit(function(e){
+            _form.find('input').each(function(e){
+                var _input = $(this);
+                var value = _input.val();
+                var error = false;
+                if(value){                        
+                    var type = this.getAttribute('type');                                     
+                    var pattern = type == 'text' ? _input.attr('pattern') : type == 'email' ? /^([a-z0-9_\.\-\+]+)@([\da-z\.\-]+)\.([a-z\.]{2,6})$/i : type == 'number' ? /^-?[0-9]*(\.[0-9]+)?$/ : type == 'url' ? /^(https?:\/\/)?[\da-z\.\-]+\.[a-z\.]{2,6}[#\?\/\w \.\-=]*$/i : false;
+                    error = pattern && !pattern.test(value);                                                                         
+                }
+                else{
+                   error = _input.attr('required');
+                }                               
+                error ? _input.addClass('error') : _input.hasClass('error') && _input.removeClass('error');
+            }); 
+           _form.find('.error').length && e.preventDefault();
+        });
+    });
+}  
+
+$(function(){
+    $('form').formValidator();
+});
