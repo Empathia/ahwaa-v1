6f717849111be07764d53f3ff31534d4d2b06e7c
felix <cf@freshout.us>
2010-10-04 11:52:58 -0700

    added the sign up and login form js

--- public/javascripts/sitescript.js
+++ public/javascripts/sitescript.js
@@ -1,4 +1,18 @@
 $(function(){
-    $('p').comments({color: '#FFFF00'});
+    $('article > p').comments({color: '#FFFF00'});
     $('.experts-carrousel > ul').blockSlider();
+    $('.sign-up > a').click(function(){
+        $('.sign-up > a').add('.over-form').removeClass('active');
+        var link = $(this);
+        link.addClass('active');
+        $('.' + link.attr('id') + '-form').addClass('active').offset({
+            'left': link.offset().left,
+            'top': link.offset().top + link.outerHeight()
+            }
+        );
+    });  
+    
+    $(document).click(function(e){                 
+        !$(e.target).hasClass('active') && !$(e.target).closest('.active').length && $('.sign-up > a').add('.over-form').removeClass('active');
+    });
 });
