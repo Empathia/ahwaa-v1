992d9db4d4df8016df1a775fabdaab9cca156f4e
felix <cf@freshout.us>
2010-10-11 14:16:33 -0700

    added missing icons on the topic page

--- public/javascripts/comments.js
+++ public/javascripts/comments.js
@@ -16,6 +16,7 @@ $.fn.comments = function(options){
                 });
             });  
             $(this).text(I18n.t('topics.show.sidebar.show_all_responses')).removeClass('hide');
+            $('.topic-content .icn-plus').removeClass('minus');
         }
         else{
             expand();
@@ -26,25 +27,19 @@ $.fn.comments = function(options){
     });
         
     $('.icn-plus').live('click', function(e){
-        var link = $(this);        
-        var parag = link.parent();         
-        var index = $('.topic-content .icn-plus').index(this);
-        var comments = parag.next('#comments_' + index + '_clone').length ? parag.next() : parag.next('#comments_add_' + index + '_clone');
-        if(comments.length){                                                  
-            comments.slideToggle(function(){
-                var paragEnd = comments.next();
-                parag.append(paragEnd.html());
-                paragEnd.remove();
-                comments.remove();
-                link.attr('style', '').css('backgroundPosition', '0 0');
-            });
+        var link = $(this), 
+            parag = link.parent(), 
+            index = $('.topic-content .icn-plus').index(this),
+            comments = parag.next('#comments_' + index + '_clone').length ? parag.next() : parag.next('#comments_add_' + index + '_clone');
+        if(comments.length){   
+            slideUpComments(comments, parag, link);                                               
         }
         else{                       
             var has_comments = link.hasClass('has_comments');                                                          
             comments = has_comments ? $('#comments_' + index).outerHTML() : $('#add_comments').outerHTML();
-            has_comments && link.css('backgroundPosition', '0 -19px');
-            var linkOuterHTML = link.css('display', 'inline').outerHTML();
-            var chunks = parag.html().split(linkOuterHTML);
+            has_comments && link.addClass('minus');
+            var linkOuterHTML = link.css('display', 'inline').outerHTML(),
+                chunks = parag.html().split(linkOuterHTML);
             parag.html(chunks[0] +  linkOuterHTML).after(comments + '<p>' + chunks[1] + '</p>');
             parag.next().attr('id',  has_comments ? 'comments_' + index + '_clone' : 'comments_add_' + index + '_clone').slideDown().addClass('clon');
         }
@@ -52,27 +47,45 @@ $.fn.comments = function(options){
         return false;
     });    
     
+    $('.cancel-comment').live('click', function(e){   
+        var comments = $(this).closest('.add_comments');
+        slideUpComments(comments, comments.prev());
+        e.preventDefault();
+        return false;        
+    })               
+    
+    function slideUpComments(comments, parag, link){
+        comments.slideToggle(function(){
+            var paragEnd = comments.next();
+            parag.append(paragEnd.html());
+            paragEnd.remove();
+            comments.remove();
+            link && link.attr('style', '').removeClass('minus');
+        }); 
+    }   
+    
     function expand(){
         $('.comments').each(function(){       
-            var comments = $(this);    
-            var id =  comments.attr('id').split('_')[1];
+            var comments = $(this),   
+                id =  comments.attr('id').split('_')[1];
             if($('#comments_' +  id +'_clone').length == 0){
-                var link = $('.topic-content .icn-plus:eq(' + id + ')')                
-                var parag = link.parent();
-                var linkOuterHTML = link.outerHTML();
-                var chunks = parag.html().split(linkOuterHTML);
+                var link = $('.topic-content .icn-plus:eq(' + id + ')'),
+                    parag = link.parent(),
+                    linkOuterHTML = link.outerHTML(),
+                    chunks = parag.html().split(linkOuterHTML);
                 parag.html(chunks[0] +  linkOuterHTML).after(comments.outerHTML() + '<p>' + chunks[1] + '</p>');
                 parag.next('#comments_' + id).attr('id',  id + '_clone').addClass('clon');
             } 
         });                                     
-        $(".comments[id$='clone']").slideDown(); 
+        $(".comments[id$='clone']").slideDown();    
+        $('.topic-content .icn-plus').addClass('minus');
     }
     var i=0;
     this.each(function(){                                             
-        var parag = $(this);
-        var paragHTML= parag.html();
-        var exp = /\.((?: [A-Z])|$)/;
-        var has_comments = "";     
+        var parag = $(this),
+            paragHTML= parag.html(),
+            exp = /\.((?: [A-Z])|$)/,
+            has_comments = "";     
         while(exp.test(paragHTML)){
             paragHTML = $('#comments_' + i).length ? paragHTML.replace(exp, ". <a href='#' class='icn-plus has_comments' title='" + I18n.t('topics.show.contextual.add_comment') + "'>&nbsp;</a>$1") :
                         paragHTML.replace(exp, "<a href='#' class='icn-plus no_comments' title='" + I18n.t('topics.show.contextual.add_comment') + "'>.</a>$1")            
@@ -84,7 +97,18 @@ $.fn.comments = function(options){
 
     $('.icn-plus').each(function(i){
        $(this).attr('id', 'add_' + i);
+    });  
+    
+    $('.new-response').live('click', function(e){
+        var addCommentForm = $('#add_comments').clone(true),
+            id = 'add_comment_clone';
+        addCommentForm.attr('id', id);
+        $(this).replaceWith(addCommentForm);
+        $('#' + id).slideDown().find('textarea').focus(); 
+        e.preventDefault();
+        return false;
     });
     
+    $()
     //expand();    
 }
