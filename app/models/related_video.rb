7e07f5eb20664eea838a1151d927af16dbf0b13d
@kidpollo <kidpollo@gmail.com>
2010-11-22 19:27:44 -0600

    small refactor

--- app/models/related_video.rb
+++ app/models/related_video.rb
@@ -27,8 +27,8 @@ class RelatedVideo < RelatedContent
   # Fetchs +title+, +description+ and +thumbnail+ from external API
   def fetch(strategy)
     strategy.parse(source_url) do |response|
-      self.title = response.title
-      self.description = response.description
+      self.title = response.title if self.title.blank?
+      self.description = response.description if self.description.blank?
       self.thumbnail = RioUtils.download(response.thumbnail_url)
     end
   end
