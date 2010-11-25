d4ff5394f370904910e6cd904464dbd1d0b4d2ae
@edgarjs <edgar.js@gmail.com>
2010-11-25 10:43:21 -0600

    Fixing js syntax for caching

--- public/javascripts/sitescript.js
+++ public/javascripts/sitescript.js
@@ -1,62 +1,62 @@
-$(function(){        
-    function toggleSignUp(link, formName){        
-        $('.auth-wrapper > a').add('.over-form').removeClass('auth-form-active');                                                              
-        link.addClass('auth-form-active');         
+$(function(){
+    function toggleSignUp(link, formName){
+        $('.auth-wrapper > a').add('.over-form').removeClass('auth-form-active');
+        link.addClass('auth-form-active');
         if(!formName){
-          formName = '.' + link.attr('id') + '-form';  
-        }     
+          formName = '.' + link.attr('id') + '-form';
+        }
         $(formName).addClass('auth-form-active').offset({
             'left': link.offset().left,
             'top': link.offset().top + link.outerHeight() - 1
             }
-        );   
-    }                
-    
+        );
+    }
+
     $('.auth-wrapper > a').click(function(){
         toggleSignUp($(this));
         return false;
-    });               
-    
-    $('#forgot-pass').click(function(){        
+    });
+
+    $('#forgot-pass').click(function(){
         $('#forgot-pass').addClass('auth-form-active');
-        toggleSignUp($('#login'), '.' + $(this).attr('id') + '-form');       
+        toggleSignUp($('#login'), '.' + $(this).attr('id') + '-form');
         return false;
     });
-    
+
     $('.sign-up-btn').live('click', function(){
         var btn = $(this);
         btn.addClass('auth-form-active').closest('.request-error').removeClass('auth-form-active');
-        toggleSignUp($('#sign-up'));         
+        toggleSignUp($('#sign-up'));
         return false;
     });
-    
+
     $('.request-topic').click(function(e){
-       var _btn = $(this);   
+       var _btn = $(this);
        if(_btn.hasClass('disabled')){
            _btn.addClass('auth-form-active');
            $('.auth-form-active').removeClass('auth-form-active');
            var offset = _btn.offset();
-           $('.request-error').addClass('auth-form-active').css({'top':offset.top+27, 'left':offset.left-39})
+           $('.request-error').addClass('auth-form-active').css({'top':offset.top+27, 'left':offset.left-39});
            e.preventDefault();
            return false;
        }
     });
-    
-    $(document).unbind('click').click(function(e){                 
+
+    $(document).unbind('click').click(function(e){
         !$(e.target).hasClass('auth-form-active') && !$(e.target).closest('.auth-form-active').length && $('.auth-form-active').removeClass('auth-form-active');
-        if(e.target.tagName != "a"){$('.search_results').removeClass('visible')}
+        if(e.target.tagName != "a"){ $('.search_results').removeClass('visible'); }
     });
-    
+
     $('.cancel-forgot').click(function(){
-        $('.auth-form-active').removeClass('auth-form-active');   
-        toggleSignUp($('#login'));       
+        $('.auth-form-active').removeClass('auth-form-active');
+        toggleSignUp($('#login'));
         return false;
     });
-    
+
     $('.sign-up-form').submit(function(){
         var that = $(this);
-        that.find('.error').remove(); 
-        
+        that.find('.error').remove();
+
         $.ajax({
             url: this.action,
             dataType: 'json',
@@ -76,24 +76,24 @@ $(function(){
                 }
             }
         });
-        
+
         return false;
     }).find('input[type=submit]').formValidator(
         {
-            'errors': {  
+            'errors': {
                 'email': I18n.t('layouts.application.header.sign_up_form.error_email_empty'),
                 'text': I18n.t('layouts.application.header.sign_up_form.error_username_empty')
             }
         }
     );
-    
+
     $('.sign-up-form #user_email').change(function () {
         $('.sign-up-form #user_username').val($(this).val().replace(/@.*$/, ''));
     });
 
     $('.login-form').submit(function(){
         var that = $(this);
-        that.find('.error').remove(); 
+        that.find('.error').remove();
         $.ajax({
             url: this.action,
             dataType: 'json',
@@ -114,11 +114,11 @@ $(function(){
         return false;
     }).find('input[type=submit]').formValidator(
         {
-            'errors': {  
+            'errors': {
                 'text': I18n.t('layouts.application.header.login_form.error_login_empty'),
                 'password': I18n.t('layouts.application.header.sign_up_form.error_password_empty')
             }
-        }   
+        }
     );
 
     $('.forgot-pass-form').submit(function () {
@@ -153,17 +153,17 @@ $(function(){
         return false;
     }).find('input[type=submit]').formValidator(
         {
-            'errors': {  
+            'errors': {
                 'text': I18n.t('layouts.application.header.forgot_pass_form.empty')
             }
-        }   
+        }
     );
 
     $('.over-form').find('input').keyup(function(){
         var input = $(this);
         $.trim(input.val()) && input.next('.auth-form-error').remove();
-    })
-    
+    });
+
     $('.search').find('input').keyup(function(){
         var input = $(this);
         $.trim(input.val()) ? input.next().addClass('clear') : input.next().removeClass('clear') && $('.search_results').removeClass('visible');
@@ -172,29 +172,31 @@ $(function(){
         _magnify.hasClass('clear') && _magnify.removeClass('clear') && _magnify.prev().val(' ').focus();
         $('.search_results').removeClass('visible');
     });
-    
-    $(".request-topic.active").pageSlide({ width: "556px", direction: "left", modal: true }).click(function(){$('.pageslide-body-wrap').addClass('jlo')});
-    
+
+    $(".request-topic.active").pageSlide({ width: "556px", direction: "left", modal: true }).click(function(){
+        $('.pageslide-body-wrap').addClass('jlo');
+    });
+
     $('.send-private-msg').click(function(){
         if(!$(this).hasClass('disabled')) {
             $(this).addClass('disabled').closest('.private-msg').children('form').slideDown();
         }
        return false;
-    });                                                               
+    });
 
     $('.private-msg').find('.cancel').click(function(){
        $(this).closest('.private-msg').find('form').slideUp().end().find('.send-private-msg').removeClass('disabled');
     });
-    
+
     $('.avatar').live('mouseover',function(){
-                $(this).siblings('.private-msg').removeClass('inside');
-                if($("body").width() < 1372 && $(this).parent().parent().is('div:first-child') || $(this).parents().hasClass('topics')) {
-                    $(this).siblings('.private-msg').fadeIn('fast').addClass('inside').addClass('active-avatar');
-                }
-                else{
-                    $(this).siblings('.private-msg').fadeIn('fast').addClass('active-avatar');
-                }
-            })
+        $(this).siblings('.private-msg').removeClass('inside');
+        if($("body").width() < 1372 && $(this).parent().parent().is('div:first-child') || $(this).parents().hasClass('topics')) {
+            $(this).siblings('.private-msg').fadeIn('fast').addClass('inside').addClass('active-avatar');
+        }
+        else{
+            $(this).siblings('.private-msg').fadeIn('fast').addClass('active-avatar');
+        }
+    });
 
     $('.active-avatar').live('mouseleave', function(){
        var pm = $(this) ;
@@ -202,30 +204,30 @@ $(function(){
          pm.find('.pm-flash').remove();
        });
     });
-    
+
     //function to place the extra topic tags into the "more" vertical list
     var lisTotal = $('#header-tags').children().length;
     var lisIndex = lisTotal;
     var lisWidth = 0;
-    
+
     /*$('#header-tags > li').each(function(e){
         lisWidth = lisWidth + $('#header-tags > li').eq(-lisIndex).width();
         lisIndex = lisIndex-1;
         console.log(lisWidth);
     })*/
-    
-    
+
+
     for (var count = 0; count < lisTotal; count++){
         lisWidth = lisWidth + $('#header-tags > li').eq(-lisIndex).width();
         lisIndex = lisIndex-1;
         //console.log(lisWidth);
     }
-    
+
     $('.items > div:last-child').css("background","none");
-    
+
     if (!$.browser.webkit) {
         var inputs = $('input:text');
-        
+
         inputs.each(function(){
             if($(this).attr('placeholder') && $(this).attr('placeholder').length > 0){
                 $(this).addClass('placeholder');
@@ -235,13 +237,13 @@ $(function(){
                     if ($(this).attr('value') == $(this).attr('placeholder')){
                         $(this).removeClass('placeholder').attr('value', ' ');
                     }
-                })
+                });
                 inputs.blur(function(){
                     if($(this).attr('value').length <= 1){
                         $(this).addClass('placeholder').attr('value', $(this).attr('placeholder'));
                     }
-                })
+                });
             }
-        })
+        });
      }
-})
+});
