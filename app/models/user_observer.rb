b3a40e48fce236138c288af3e20bb32aca0bfd26
@kidpollo <kidpollo@gmail.com>
2010-12-09 13:08:40 -0600

    removed user observer that sets default level

--- app/models/user_observer.rb
+++ app/models/user_observer.rb
@@ -1,9 +1,6 @@
 class UserObserver < ActiveRecord::Observer
 
   def after_create(user)
-    Reward.descendants.each do |klass|
-      user.set_reward klass.first if klass.first
-    end
   end
 
 end
--- spec/models/user_observer_spec.rb
+++ spec/models/user_observer_spec.rb
@@ -1,14 +1,4 @@
 require 'spec_helper'
 
 describe UserObserver do
-  it "should set initial rewards" do
-    @level = mock_model(Level)
-    Level.stub!(:first).and_return @level
-    Badge.stub!(:first).and_return @level
-    Prize.stub!(:first).and_return @level
-    @user = mock_model(User)
-    @obs = UserObserver.instance
-    @user.should_receive(:set_reward).exactly(3).times
-    @obs.after_create @user
-  end
 end
