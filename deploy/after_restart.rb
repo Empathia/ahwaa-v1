0fe92b32925d7c8d138929fe33de7b2662704d85
@edgarjs <edgar.js@gmail.com>
2010-12-16 11:48:30 -0600

    not delete all.js if none

--- deploy/after_restart.rb
+++ deploy/after_restart.rb
@@ -1,7 +1,8 @@
 env = node['environment']['framework_env']
 current_path = "/data/transcend/current"
+all_js = "#{current_path}/public/javascripts/all.js"
 
 puts "Generating babilu locales for javascript"
 run "cd #{current_path} && rake babilu:generate RAILS_ENV=#{env}"
-run "rm #{current_path}/public/javascripts/all.js"
+run "rm #{all_js}" if ::File.exists?(all_js)
 
