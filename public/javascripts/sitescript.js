74dc858484c015ab07e4376cf5edf69afdb772cd
arruti <arruti@freshout.us>
2010-12-16 11:48:39 -0600

    heart z-index on user-card

--- public/javascripts/sitescript.js
+++ public/javascripts/sitescript.js
@@ -1,5 +1,5 @@
 $(function(){   
-    
+
     function toggleSignUp(link, formName){
         $('.auth-wrapper > a').add('.over-form').removeClass('auth-form-active').filter('.over-form').addClass('hidden');
         link.addClass('auth-form-active');
@@ -66,7 +66,7 @@ $(function(){
     $('.sign-up-form #user_email').change(function () {
         $('.sign-up-form #user_username').removeClass('placeholder').val($(this).val().replace(/@.*$/, ''));
     });
-        
+
     $('.login-form').find('input[type=submit]').formValidator({
         'errors': {
             'text': I18n.t('layouts.application.header.login_form.error_login_empty'),
@@ -157,6 +157,7 @@ $(function(){
            classes += "inside";
         }
         $(this).siblings('.private-msg').fadeIn('fast').addClass(classes);
+        $(this).find('span').css('z-index','100000');
     });
 
     $('.active-avatar').live('mouseleave', function(){
@@ -170,9 +171,8 @@ $(function(){
         $(this).hasClass('error') && $(this).removeClass('error') && $(this).siblings('p.error').remove();
     })
 
-
     $('.items > div:last-child').css("background","none");
-    
+
     if (!$.browser.webkit) {
         var inputt = $('input:text[placeholder]'),
             inputp = $('input:password[placeholder]'),
@@ -193,8 +193,8 @@ $(function(){
                     input.attr('value').length == 0 && input.addClass('placeholder').attr('value', input.attr('placeholder'));
                 })
             }
-        });     
-        
+        });
+
         inputp.each (function placeimg(){         
             var input = $(this);
             input.attr('placeholder') && input.attr('placeholder').length > 0 && input.val() == 0 && input.addClass('placeimg');
@@ -209,7 +209,7 @@ $(function(){
             if (event.keyCode == 27) $.fn.pageSlideClose();
         });
      }
-                 
+
      var flashAlert = $('.flash.alert');
      if(flashAlert.length){
         setTimeout(function(){                                                            
@@ -220,7 +220,7 @@ $(function(){
                   })
                 }); 
             });
-        }, 1000);             
+        }, 1000);
      }
 });
 
@@ -228,7 +228,7 @@ $(window).load(function () {
     $(".request-topic.active").pageSlide({ width: "556px", direction: "left", modal: true }).click(function(){
         $('.pageslide-body-wrap').addClass('jlo');
     });
-    
+
     var headerTags = $('#header-tags'),
         moreTags = $('#moretags'),
         tag = null,
@@ -245,14 +245,14 @@ $(window).load(function () {
     if($.browser.msie){
         $.browser.version == '8.0' ? $('.over-form').addClass('hidden') : headerTags.parent().css('position', 'static');
     }
-      
+
     var container = $('.container'),
         siblings = container.siblings(),
         header = siblings.filter('header'),
         footer = siblings.filter('footer'),
         delta = header.outerHeight() + footer.outerHeight() + 50;
     container.css('min-height', self.height() - delta);
-    
+
     self.resize(function(){
         container.css('min-height', self.height() - delta);
     });
