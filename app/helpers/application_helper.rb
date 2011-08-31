1df0f5d84b8500a2b609b90e02157958df30f874
edgarjs <edgar.js@gmail.com>
2011-08-31 09:32:13 -0500

    Fix anonymous actions from stream

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -56,7 +56,7 @@ module ApplicationHelper
 
   # get acatar css for user
   def level_css_class_for_user(user)
-    if user.current_level
+    if user && user.current_level
       user.current_level.name.underscore.parameterize
     else
       'no-level'
