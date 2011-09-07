9ac67657effab2e225078d5f2a61da6a702002a2
Noel Delgado <auhcsei@gmail.com>
2011-09-07 11:49:09 -0500

    User Stream Send Message form markup + behavior

--- public/javascripts/user-nav.js
+++ public/javascripts/user-nav.js
@@ -2,4 +2,14 @@ $(function(){
     $('.panel-sub-nav .sub-nav > li').last().addClass("last-child");
     $('.panel-sub-nav .sub-nav > li.active').prev().css("border-bottom", "0 none");
     $('.panel-sub-nav .sub-nav > li.active').next().css("border-top", "0 none");
+    
+    $('.js-stream-send-msg').bind('click', function() {
+        $(this).siblings('.send-from-stream').fadeIn(200);
+        return false;
+    })
+    
+    $('.send-from-stream .cancel').bind('click', function() {
+        $(this).parents('.send-from-stream').fadeOut(200);
+        return false;
+    })
 });
