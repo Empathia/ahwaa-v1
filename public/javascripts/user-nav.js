80242aa3c06cdf963ca622181d6e03ecd60f7a56
Noel Delgado <auhcsei@gmail.com>
2011-09-07 12:12:45 -0500

    User Stream Send Message form Submit

--- public/javascripts/user-nav.js
+++ public/javascripts/user-nav.js
@@ -4,12 +4,7 @@ $(function(){
     $('.panel-sub-nav .sub-nav > li.active').next().css("border-top", "0 none");
     
     $('.js-stream-send-msg').bind('click', function() {
-        $(this).siblings('.send-from-stream').fadeIn(200);
-        return false;
-    })
-    
-    $('.send-from-stream .cancel').bind('click', function() {
-        $(this).parents('.send-from-stream').fadeOut(200);
+        $(this).siblings('.private-msg.send-from-stream').fadeIn(200);
         return false;
     })
 });
