7ae73b413add59509a60ff153f3c3df9bb8f0f15
MohamedAlaa <m@freshout.us>
2011-10-07 15:33:34 -0500

    Creating New English Header

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -3,5 +3,9 @@ $(function(){
         $('#pageslide-body-wrap').css('position', 'static');
         $(this).trigger('clickOverlay');
     });
-});
 
+    $('.video-player .preview-video').hide();
+    $('.video-player .preview-image > a').bind('mousedown', function() {
+  		$(this).parent().hide().siblings('.preview-video').show();
+	});
+});
