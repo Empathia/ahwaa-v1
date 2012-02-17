acaa7a9556a336eec73e805a5a1bef543560f7bf
Emmanuel Medina <minardimedia@gmail.com>
2012-02-17 17:09:40 -0600

    Adding hansel-gretel and component ids

---
+++ public/javascripts/include/hansel.js
@@ -0,0 +1,52 @@
+	window.hansel = {};
+	 
+	// set api token
+	window.hansel.token = 'YXlTNDlJd3lHcjR5X3phY3dxalFZdw==:ZnJlc2hvdXQtYm90:VJ_6x1p3kedy7Hem4TdwTQ==';
+
+	// callback
+	//window.hansel.saveDataCallback = function(ev) {
+		// do magic with the event information
+		// and store whatever you want in an obj (var data = {})
+		// return data
+	//}
+
+	// callback
+	//window.hansel.saveInitialDataCallback = function() {
+		// save special data from the initial state
+		// by default the complete markup is store
+		// return {key:val} or return data
+	//}
+
+	window.gretel = {}
+
+	// callback
+  window.gretel.appGetComponent = function(step) {
+    // get the component according with currentStep at that moment
+    // return the dom node of that component
+    return $('[data-hansel-component-id=' + step.component + ']')[0];
+    //return {
+  }
+
+	window.gretel.appSetInitialState = function(environment) {
+		// the environment object holds all the data saved from the
+		// app when the initial state was saved
+		// resolution, browser, os, and also the initial state
+		// saved using the window.hansel.saveInitialDataCallback fn if it was used
+		// This function will return an object that will be evaluated by
+		// a watir (http://watir.com/) to set the browser to the same initial state
+		// as the user
+		// return obj
+	}
+	// params needed to set the correct urls to get and post data
+	// to ginger (backend)
+	window.gretelParams = {};
+	gretelParams.gretelArray = window.document.referrer.replace(/https?:\/\//,'').split('/');
+	gretelParams.gretelOpen = gretelParams.gretelArray[1] == 'launch' ? true : false;
+	if (gretelParams.gretelOpen) {
+		gretelParams.inicidenID = gretelParams.gretelArray[2];
+		gretelParams.errorID = gretelParams.gretelArray[3];
+		document.write(unescape("%3Cscript src='http://50.16.153.9/js/gretel.js' type='text/javascript'%3E%3C/script%3E"));
+	} else {
+		document.write(unescape("%3Cscript src='http://50.16.153.9/js/hansel.js' type='text/javascript'%3E%3C/script%3E"));
+	}
+
