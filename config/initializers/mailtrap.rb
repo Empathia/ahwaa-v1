b763e09fc68553f05a9586d8dd11883057f1ccff
Adrian Castillo <rodacato@gmail.com>
2012-08-20 15:45:19 -0500

    Removed production validation

--- config/initializers/mailtrap.rb
+++ config/initializers/mailtrap.rb
@@ -1,10 +1,8 @@
-if !Rails.env.production?
-  ActionMailer::Base.delivery_method = :smtp
-  ActionMailer::Base.smtp_settings = {
-    :user_name => "lgbt",
-    :password => "4eebfdaa63683435",
-    :address => "mailtrap.io",
-    :port => 2525,
-    :authentication => :plain,
-  }
-end
+ActionMailer::Base.delivery_method = :smtp
+ActionMailer::Base.smtp_settings = {
+  :user_name => "lgbt",
+  :password => "4eebfdaa63683435",
+  :address => "mailtrap.io",
+  :port => 2525,
+  :authentication => :plain,
+}
