2ed26c065b536c8b4154d097ea86846666e23195
Adrian Castillo <rodacato@gmail.com>
2012-08-28 12:33:41 -0500

    Move interest topics declaration to its own method

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -14,7 +14,7 @@ class HomeController < ApplicationController
     stream_users = @user.filtered_stream_users(params[:filter], I18n.locale)
 
     @users_similar_profile = User.suggestions_with_similar_profile(current_user.profile)
-    similar_topics_ids = Topic.by_tags_in((current_user.subscriptions + current_user.visited_topics.limit(4)).map(&:topic_id).uniq).map(&:topic_id)
+    similar_topics_ids = Topic.of_interest_for(current_user)
     @users_similar_interests = similar_topics_ids.empty? ? [] : User.suggestions_with_similar_topics(current_user, similar_topics_ids.join(',') )
 
     if request.format == :html && !request.xhr?
