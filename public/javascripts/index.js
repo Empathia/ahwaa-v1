9467cfe20bed022c206082fd457fb9470530508d
Noel Delgado <auhcsei@gmail.com>
2013-07-03 17:03:14 -0500

    removing jquery.overlay and references (not used anymore)

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,9 +1,4 @@
 $(function(){
-    $('.watch-video a').overlay({href: '#watch-video-overlay'}).click(function(e){
-        $('#pageslide-body-wrap').css('position', 'static');
-        $(this).trigger('clickOverlay');
-    });
-
     $('.video-player .preview-video').hide();
     $('.video-player .preview-image > a').bind('mousedown', function() {
   		$(this).parent().hide().siblings('.preview-video').show();
