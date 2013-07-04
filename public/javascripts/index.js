e8434e06e825a1c54567d5e57c973efcbcad348a
Noel Delgado <auhcsei@gmail.com>
2013-07-04 12:14:26 -0500

    home : video responsive, topics layout fix - (progress)

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,6 +1,6 @@
 $(function(){
     $('.video-player .preview-video').hide();
-    $('.video-player .preview-image > a').bind('mousedown', function() {
-  		$(this).parent().hide().siblings('.preview-video').show();
-	});
+ //    $('.video-player .preview-image > a').bind('click', function() {
+ //  		$(this).parent().hide().siblings('.preview-video').show();
+	// });
 });
