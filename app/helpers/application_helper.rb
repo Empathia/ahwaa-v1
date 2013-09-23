57b17f8157af6ca8337478fccdee57ad301f408b
Alejandro <alejandro@freshout.us>
2013-09-23 15:37:12 -0500

    truncate html

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -63,4 +63,45 @@ module ApplicationHelper
       'no-level'
     end
   end
+  def truncate_html(input, *args)
+    require "rexml/parsers/pullparser"
+    # support both 2.2 & earlier APIs
+    options = args.extract_options!
+    length = options[:length] || args[0] || 30
+    omission = options[:omission] || args[1] || '&hellip;'
+
+    begin
+      parser = REXML::Parsers::PullParser.new(input)
+      encoder = HTMLEntities.new(TruncateHtmlHelper.flavor)
+      tags, output, chars_remaining = [], '', length
+
+      while parser.has_next? && chars_remaining > 0
+        element = parser.pull
+        case element.event_type
+        when :start_element
+          output << rexml_element_to_tag(element)
+          tags.push element[0]
+        when :end_element
+          output << "</#{tags.pop}>"
+        when :text
+          text = encoder.decode(element[0])
+          output << encoder.encode(text.first(chars_remaining))
+          chars_remaining -= text.length
+          output << omission if chars_remaining < 0
+        end
+      end
+
+      tags.reverse.each {|tag| output << "</#{tag}>" }
+      output
+
+    rescue REXML::ParseException => e
+      fixed_up = Nokogiri::HTML.fragment(input).to_html
+      raise ::TruncateHtmlHelper::InvalidHtml, "Could not fixup invalid html. #{e.message}" if fixed_up == input
+      input = fixed_up
+      retry
+    end
+  end
+  def rexml_element_to_tag(element)
+    "<#{element[0]}#{element[1].inject(""){|m,(k,v)| m << %{ #{k}="#{v}"}} unless element[1].empty?}>"
+  end
 end
